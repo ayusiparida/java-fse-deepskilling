f5d2519b181813655d3f8a0ed0f6238cb2b02b1f
ayushi08122004 <ayushiparida2004@gmail.com>
2026-07-02 16:58:21 +0530

    Add PL SQL scenarios and junitdemo project

---
+++ junitdemo/src/test/java/SampleTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class SampleTest {
+
+    @Test
+    public void testAddition() {
+        assertEquals(5, 2 + 3);
+    }
+}
