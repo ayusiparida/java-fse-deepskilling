686c6d7d438ad294e6efd865f859f9d5fb6a5a6c
ayushi08122004 <ayushiparida2004@gmail.com>
2026-07-09 18:55:58 +0530

    Added Java FSE Deep Skilling solutions

---
+++ Mockito Exercises/Exercise 2/src/test/java/com/example/VerifyInteractionTest.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+
+public class VerifyInteractionTest {
+
+    @Test
+    void testVerifyInteraction() {
+
+        ExternalApi mockApi = mock(ExternalApi.class);
+
+        MyService service = new MyService(mockApi);
+
+        service.fetchData();
+
+        verify(mockApi).getData();
+    }
+}
